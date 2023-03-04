6e2a4c14ce53021446403003655b1c93a9cda902
Muhammad Ahsan <59480396+DevAhsaNOp@users.noreply.github.com>
2023-03-04 16:21:33 +0500

    Updated!

---
+++ DMS_BOL/Validation Classes/ValidateDoctorServices.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DMS_BOL.Validation_Classes
+{
+    public class ValidateDoctorServices
+    {
+        public int DS_ID { get; set; }
+
+        [Required(ErrorMessage ="Provide Doctor ID")]
+        [Display(Name ="Doctor ID")]
+        public int? DS_DoctorID { get; set; }
+
+        [Required(ErrorMessage = "Provide Services ID")]
+        [Display(Name = "Services ID")]
+        public int? DS_ServicesID { get; set; }
+
+        public DateTime? DS_CreatedOn { get; set; }
+
+        [Required(ErrorMessage = "Provide Created By")]
+        [Display(Name = "Created By")]
+        public int? DS_CreatedBy { get; set; }
+
+        public DateTime? DS_UpdatedOn { get; set; }
+
+        [Required(ErrorMessage = "Provide Updated By")]
+        [Display(Name = "Updated By")]
+        public int? DS_UpdatedBy { get; set; }
+        public bool? DS_IsActive { get; set; }
+        public bool? DS_IsArchive { get; set; }
+
+        public virtual tblDoctor tblDoctor { get; set; }
+        public virtual tblService tblService { get; set; }
+    }
+}
---
+++ DMS_BOL/Validation Classes/ValidateDoctorWorkExperience.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DMS_BOL.Validation_Classes
+{
+    public class ValidateDoctorWorkExperience
+    {
+        public int WEX_ID { get; set; }
+
+        [Required(ErrorMessage = "Provide Doctor ID")]
+        [Display(Name = "Doctor ID")]
+        public int? WEX_DoctorID { get; set; }
+
+        [Required(ErrorMessage = "Provide Hospital Name")]
+        [Display(Name = "Hospital Name")]
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Only Alphabets are allow")]
+        public string WEX_HospitalName { get; set; }
+
+        [Required(ErrorMessage = "Provide your Designation")]
+        [Display(Name = "Designation")]
+        [RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Only Alphabets are allow")]
+        public string WEX_Designation { get; set; }
+
+        [Required(ErrorMessage = "Provide your Still Working")]
+        [Display(Name = "Still Working")]
+        public bool? WEX_IsWorking { get; set; }
+
+        [Required(ErrorMessage = "Provide your Start Working Date")]
+        [Display(Name = "Start Working Date")]
+        public DateTime? WEX_FromDate { get; set; }
+
+        [Display(Name = "End of Working Date")]
+        public DateTime? WEX_ToDate { get; set; }
+
+        public bool? WEX_IsActive { get; set; }
+
+        public bool? WEX_IsArchive { get; set; }
+
+        [Required(ErrorMessage = "Provide Created By")]
+        [Display(Name = "Created By")]
+        public int? WEX_CreatedBy { get; set; }
+
+        public DateTime? WEX_CreatedOn { get; set; }
+
+        [Required(ErrorMessage = "Provide Updated By")]
+        [Display(Name = "Updated By")]
+        public int? WEX_UpdatedBy { get; set; }
+
+        public DateTime? WEX_UpdatedOn { get; set; }
+
+        public virtual tblDoctor tblDoctor { get; set; }
+    }
+}
